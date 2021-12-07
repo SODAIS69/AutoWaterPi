d175c3034af09908f17253aebe386c6857f981e6
sodais69 <6829907+SODAIS69@users.noreply.github.com>
2021-12-07 20:04:30 +0800

    fix gallary cant genarate indxe.html if not existed.

--- gallery.py
+++ gallery.py
@@ -1,17 +1,18 @@
 import os
-basepath=f'{os.path.dirname(os.path.abspath(__file__))}/Captured/'
-pictures=os.listdir(basepath)
-pictures.remove('index.html')
-picturesSort=sorted(pictures,reverse=True)
-htmlhead="<html>\r\n<head>\r\n<title>My dear plant</title>\r\n<style>\r\ndiv.gallery {\r\nmargin: 5px;\r\nborder: 1px solid #ccc;\r\nfloat: left;\r\nwidth: 400px;\r\n}\r\n\r\ndiv.gallery:hover {\r\nborder: 1px solid #777;\r\n}\r\n\r\ndiv.gallery img {\r\nwidth: 100%;\r\nheight: auto;\r\n}\r\n\r\ndiv.desc {\r\npadding: 15px;\r\ntext-align: center;\r\n}\r\n</style>\r\n</head>\r\n<body>\r\n"
-htmlbottom="</body>\r\n\r\n</html>"
+def GenerateGallery():
+    basepath=f'{os.path.dirname(os.path.abspath(__file__))}/Captured/'
+    pictures=os.listdir(basepath)
+    if os.path.exists(f'{basepath}/index.html'):
+        pictures.remove('index.html')
+    picturesSort=sorted(pictures,reverse=True)
+    htmlhead="<html>\r\n<head>\r\n<meta http-equiv=\"refresh\" content=\"20\" ><meta charset=\"utf-8\"><title>My dear plant</title>\r\n<style>\r\nbody {background-color: #dae0ff;}div.gallery {\r\n background-color:white; margin: 5px;\r\nborder: 1px solid #ccc;\r\nfloat: left;\r\nwidth: 400px;\r\n}\r\n\r\ndiv.gallery:hover {\r\nborder: 1px solid #777;\r\n}\r\n\r\ndiv.gallery img {\r\nwidth: 100%;\r\nheight: auto;\r\n}\r\n\r\ndiv.desc {\r\npadding: 15px;\r\ntext-align: center;\r\n}\r\n</style>\r\n</head>\r\n<body>\r\n<h1>~我ㄉ盆栽~</h1>"
+    htmlbottom="</body>\r\n\r\n</html>"
 
-for i in picturesSort:
-    htmlhead+=f"<div class=\"gallery\">\r\n<a target=\"_blank\" href=\"{i}\">\r\n<img src=\"{i}\" alt=\"Cinque Terre\" width=\"800\">\r\n</a>\r\n<div class=\"desc\">{i}</div>\r\n</div>"
-htmlhead+=htmlbottom
+    for i in picturesSort:
+        htmlhead+=f"<div class=\"gallery\">\r\n<a target=\"_blank\" href=\"{i}\">\r\n<img src=\"{i}\" alt=\"Cinque Terre\" width=\"800\">\r\n</a>\r\n<div class=\"desc\">🌻:&nbsp;{i}</div>\r\n</div>"
+    htmlhead+=htmlbottom
+    #print(f"{basepath}/index.html")
+    f=open(f'{basepath}/index.html',"w")
+    f.write(htmlhead)
+    f.close()
 
-f=open(f'{basepath}/index.html',"w")
-f.write(htmlhead)
-f.close()
-
-print(pictures)
