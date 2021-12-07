c392dfa8a737929422fc4a9e5ff1907a1ed0aa17
sodais69 <6829907+SODAIS69@users.noreply.github.com>
2021-12-07 20:04:51 +0800

    fix gallary cant genarate indxe.html if not existed.

--- capture.py
+++ capture.py
@@ -1,8 +1,11 @@
 import time
 from picamera import PiCamera
 import os
+import gallery
 
 camera = PiCamera()
 nowtime=int(time.time())
+camera.resolution = (1640, 1232)
 timestamp=time.strftime("%Y-%m-%d-%H:%M:%S",time.localtime(nowtime) )
 camera.capture(f'{os.path.dirname(os.path.abspath(__file__))}/Captured/{timestamp}.jpg') #取script路徑+資料夾
+gallery.GenerateGallery()
